43a1fc864b20c1ef014fb55e65b46cc1879c0c93
wjsacken <wsackenheim@artscube.biz>
2024-04-12 14:01:04 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
-}
+  reactStrictMode: true,
+  target: 'experimental-serverless-trace', // This is needed for Next.js 13
+  output: {
+    export: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
