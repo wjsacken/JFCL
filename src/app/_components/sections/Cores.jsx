e7809a2745a5c133493a5f6dc6d3db046e20a462
wjsacken <wsackenheim@artscube.biz>
2024-07-10 13:43:25 -0400

    Update Cores.jsx

--- src/app/_components/sections/Cores.jsx
+++ src/app/_components/sections/Cores.jsx
@@ -19,29 +19,24 @@ const CoresSection = () => {
             <div className="container mil-p-120-90">
                 <div className="row justify-content-between">
                     <div className="col-lg-12">
-
                         <div className="">
                             <span className="mil-suptitle mil-upper mil-light mil-up mil-mb-30" dangerouslySetInnerHTML={{__html : Data.subtitle}} />
                             <h2 className="mil-upper mil-light mil-up mil-mb-30" dangerouslySetInnerHTML={{__html : Data.title}} />
                             <p className="mil-light-soft mil-up mil-mb-40" dangerouslySetInnerHTML={{__html : Data.description}} />
                         </div>
-
                     </div>
                     <div className="col-lg-12 mil-mt-suptitle-offset">
                         {Data.items.map((item, key) => (
                         <div className="mil-accordion-group mil-up" key={`cores-item-${key}`}>
                             <div className="mil-accordion-menu">
-
                                 <div className="mil-symbol mil-light mil-thin mil-h3">
                                     <div className="mil-plus">+</div>
                                     <div className="mil-minus">-</div>
                                 </div>
-
                                 <h6 className="mil-upper mil-light">{item.title}</h6>
-
                             </div>
                             <div className="mil-accordion-content">
-                                <span className="mil-light-soft mil-mb-30" dangerouslySetInnerHTML={{__html : item.text}}></span>
+                                <span className="mil-light-soft" dangerouslySetInnerHTML={{__html : item.text}}></span>
                             </div>
                         </div>
                         ))}
